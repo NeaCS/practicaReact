fe775e3b1489850df0eed8c014c795872c839bfd
NeaCS <naldi.castro@gmail.com>
2022-11-11 11:35:55 -0300

    ahora si ajax

--- src/components/AjaxApi.js
+++ src/components/AjaxApi.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-function Pokemons (props) {
+function Pokemon (props) {
     return (
         <figure>
         <img src={props.avatar} alt={props.name} />
@@ -19,15 +19,42 @@ componentDidMount(){
     let url = "http://pokeapi.co/api/v2/pokemon/"
     fetch(url)
     .then(res=>res.json())
-    .then(json => console.log(json))
+    .then((json) => {
+    console.log(json);
+    json.results.forEach((el) => {
+        fetch(el.url)
+        .then(res=>res.json())
+        .then((json) => {
+            console.log(json)
+        let pokemon = {
+            id: json.id,
+            name: json.name,
+            avatar: json.sprites.front_default
+
+
+        };
+
+        let pokemons = [...this.state.pokemons, pokemon];
+        this.setState({pokemons});
+    
+    });
+        
+    });
+    
+});
     
 }
 
 
+  /* Condicional para mostrar "cargando mientras llegan los datos" */
     render() {
     return (
     <>
         <h2>Peticiones Asíncronas en Componentes de Clase</h2>
+        {this.state.pokemons.length} === 0 ? (
+            <h3>Cargando...</h3>
+        ) : (
+        {this.state.pokemons.map(el => <Pokemon key={el.id} name={el.name} avatar={el.avatar} />)})
     </>
     )
 }}
